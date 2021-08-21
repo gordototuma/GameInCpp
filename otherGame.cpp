b58ca7686bb2a858225f15c9f20c67c80867c95c
Luis Alejandro González Montañez <40329501+gordototuma@users.noreply.github.com>
2021-08-20 22:15:28 -0500

    save location

--- otherGame.cpp
+++ otherGame.cpp
@@ -1,6 +1,10 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+ifstream GameFile("GameFile.txt");
 
 
 void DrawMap(int HeroPosX, int HeroPosY,char GameMap[5][5] ){
@@ -13,14 +17,96 @@ void DrawMap(int HeroPosX, int HeroPosY,char GameMap[5][5] ){
         }
         cout<<endl;
     }   
+}
 
+
+bool FileValidation(){
+
+    bool ExistGameFile = GameFile.is_open();
+    //GameFile.close();
+    return ExistGameFile;    
+    
+}
+
+
+bool CreateGameFile(){
+    ofstream GameFile("GameFile.txt");
+    if (GameFile.is_open()){
+        GameFile<<"1"<<endl;
+        GameFile<<"1"<<endl;
+        GameFile.close();
+        return true;
+    }else{
+        cout<<"A ocurrido un error al crear archivo de posicionamiento"<<endl;
+        GameFile.close();
+        return false;
+    }
+}
+
+
+int HeroPosXFunction(){
+
+    if (FileValidation()){
+
+        //ifstream GameFile("GameFile.txt");
+        string PosX = "";
+
+        //if (GameFile.is_open()){
+            
+        getline(GameFile, PosX);  
+        //}        
+        stringstream PosXStr(PosX);
+        int PosXInt = 0;
+        PosXStr>>PosXInt;
+        GameFile.close();
+        
+        return PosXInt;
+    }
+    else{
+        if (CreateGameFile()){
+            return 1;
+        }   
+    } 
+}
+
+
+int HeroPosYFunction(){
+
+    if (FileValidation()){
+
+        //ifstream GameFile("GameFile.txt");
+        string PosY = "";
+
+        //if (GameFile.is_open()){
+        int accountancy = 0;
+        int PosYInt = 0;
+        while (getline(GameFile, PosY)){
+            //cout<<"asdasd"<<PosY<<endl;
+            if (accountancy == 1){
+                stringstream PosYStr(PosY);
+                cout<<"asdasd"<<PosY<<endl;
+                
+                PosYStr>>PosYInt;
+            }
+            accountancy++;
+        }
+        GameFile.close();
+        return PosYInt;
+        //}      
+    }
+    else{
+        if (CreateGameFile()){
+            return 1;
+        }
+    } 
 }
 
 
 int main(){
 
-    int HeroPosX = 1;
-    int HeroPosY = 1;
+    cout<<HeroPosXFunction()<<HeroPosYFunction()<<"\n";
+    int HeroPosX = HeroPosXFunction();
+    int HeroPosY = HeroPosYFunction();
     bool isGameOver = false;
     char GameMap[5][5] = {
                             {'1','1','1','1','1'},
